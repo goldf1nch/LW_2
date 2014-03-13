3ee09cb342d33ad0bcc2caf3ae7275a02c36f53f
Anna <Fiery666Snow999@gmail.com>
2014-03-13 10:13:41 +0300

    upd: add documentation files

--- LW_2.cpp
+++ LW_2.cpp
@@ -2,27 +2,26 @@
 ///
 ///UPD: adding commentaries in the style of Doxygen
 /*!UPD: Cyrillic symbols do not support? =\*/
-
+/// \file LW_2.cpp
 #include "stdafx.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale(LC_ALL, "rus");
-	printf("������������ ������ �2");
+	printf("Laboratory work 2");
 	printf_s("\n");
 	int n, buf;
 	int k=0;
-	printf_s("\n������� ����� ��������� � �������: "); scanf_s ("%d", &n);
+	printf_s("\Enter numbers of symbols: "); scanf_s ("%d", &n);
 	int *mas;
 	mas=new int [n];
-	printf_s("\n������ �� ����������: \n");
-	for (int i=0; i<n; i++) mas[i]=rand()%201-100; /// ���������� ��������� ����� ��� ������� 
-	for (int i=0; i<n; i++) printf("\n[%d]: %d", i+1, mas[i]); /// ������ �������
-	printf_s("\n\n������ ����� ����������: \n");
+	printf_s("\nUnsorting massive: \n");
+	for (int i=0; i<n; i++) mas[i]=rand()%201-100; /// ...
+	for (int i=0; i<n; i++) printf("\n[%d]: %d", i+1, mas[i]); /// ...
+	printf_s("\n\nSorting massive: \n");
 	/*!
-	�������� ���� ���� - ���������� ��������� ���������� ������� ��������.
-	������ ����� ���������� ������ ���������, ������� ��� ��������� ������ ������������ �����.
+	Commentaries in the style Doxygen...
 	*/
 	mt:for (int i=0; i<(n-1); i++)
 	{
---
+++ html/search/all_6c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lw_5f2_2ecpp',['LW_2.cpp',['../_l_w__2_8cpp.html',1,'']]]
+];
